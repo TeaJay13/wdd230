5f6d5b81322e89c31e920ac9655e9a9a1c1c7cc9
TeaJay13 <131283247+TeaJay13@users.noreply.github.com>
2024-05-09 14:01:28 -0600

    5/9 - hamburger menu update

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,6 +1,12 @@
-let menu_button = document.querySelector('.menu-button');
-let navlist = document.querySelector('.nav ul');
+let menu_button = document.querySelector('#menu-button');
+let navlist = document.querySelector('nav ul');
+let menuIcon = document.querySelector('#menu-icon');
+
 menu_button.addEventListener('click', () => {
-    menu_button.classList.toggle('open');
     navlist.classList.toggle('open');
+    if (navlist.classList.contains('open')) {
+        menuIcon.innerHTML = '&times;'; // Sets the icon to "X"
+    } else {
+        menuIcon.innerHTML = '&#x2261;'; // Sets the icon back to "="
+    }
 });
