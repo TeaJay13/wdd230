a6fc9a1a3f100e9a4e0e16771a8512c03bcfad56
TeaJay13 <131283247+TeaJay13@users.noreply.github.com>
2024-06-19 14:50:55 -0600

    discover page update

--- chamber/scripts/datemath.js
+++ chamber/scripts/datemath.js
@@ -1,16 +1,23 @@
 
-// Gets last visited date from local storage and displays it on the page, checks if it is your first visit, will dispolay day and welcome message for first day, if not it will display date and welcome back message
-let lastVisited = localStorage.getItem('lastVisited');
-if (lastVisited === null) {
-    document.getElementById('lastVisited').textContent = 'Welcome to the Chamber of Secrets! Today is: ' + currentDate + '. Enjoy your stay!';
-} else {
-    document.getElementById('lastVisited').textContent = 'Welcome back! You last visited on: ' + lastVisited + '.';
-}
+const lastVisited = localStorage.getItem('lastVisited');
+const date = new Date();
+const lastVisitedMessage = document.getElementById('lastVisited');
+const DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
 
-
-// Gets current date and time and displays it on the page
-let currentDate = new Date();
+let currentDate = date.toDateString();
 document.getElementById('currentDate').textContent = currentDate;
 
-// Saves current date and time to local storage
-localStorage.setItem('lastVisited', currentDate);
+if (!lastVisited){
+    lastVisitedMessage.textContent = 'Welcome to the Chamber of the Round Table!';
+}
+else if (date - lastVisited < DAY_IN_MILLIS){
+    lastVisitedMessage.textContent = 'Welcome back!'
+}
+else{
+    lastVisitedMessage.textContent = `Welcome back! You last visited ${Math.floor((date - lastVisited) / DAY_IN_MILLIS)}`
+}
+
+localStorage.setItem('lastVisited', date.getTime());
+
+
+
