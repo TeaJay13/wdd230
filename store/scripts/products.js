326298dbe42f012f1f58e2f0194e2dd1b500bf20
TJ Strickland <thomaskeith17@gmail.com>
2024-07-18 07:14:19 -0600

    products pages

---
+++ store/scripts/products.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", async function() {
+    const response = await fetch('data/products.json');
+    const products = await response.json();
+    const productContainer = document.querySelector('#product-list');
+
+    products.forEach(product => {
+        const productCard = `
+            <div class="product">
+                <img class="product-imgs" loading="lazy" src="${product.imageUrl}" alt="${product.name}" height="300">
+                <h3>${product.name}</h3>
+                <p>${product.description}</p>
+                <p>$${product.price.toFixed(2)}</p>
+                <button data-sku="${product.sku}">Add to Cart</button>
+            </div>
+        `;
+        productContainer.innerHTML += productCard;
+    });
+});
+
+function toggleView(viewType) {
+    const container = document.getElementById('product-list');
+    if (viewType === 'list') {
+        container.className = 'list-view'; // Set the container to list view mode
+    } else {
+        container.className = 'grid-view'; // Set the container to grid view mode
+    }
+}
